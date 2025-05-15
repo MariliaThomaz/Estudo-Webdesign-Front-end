24633ecae303ec0e27ef057423c5f081bbb24a04
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2025-05-14 21:32:07 -0300

    Studies arithmetic assignment, increment, and decrement operators in JavaScript

--- Logica JavaScript/introducao/operador_atribuicao.js
+++ Logica JavaScript/introducao/operador_atribuicao.js
@@ -7,3 +7,15 @@ x = x +50
 console.log(x)
 //---------------
 x += 50 // isto mesma coisa que x = x + 50
+
+// operador  atribuição  e aritmético
+//decremento e incremento
+let z =10
+
+++z // incremento - esta  soma 1 para a variável
+console.log("++z "+z) // 11
+z++ // incremento - esta  soma 1 para a variável este é o pós incremento
+console.log("z++ "+z) // 12    
+z-- // decremento - esta  subtrai 1 para a variável
+console.log("z-- "+z) // 11    
+
