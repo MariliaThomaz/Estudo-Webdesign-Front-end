d27e389ebd12e123376503c7006bbd5be5abdaa3
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2025-05-29 22:59:06 -0300

    Begins study of JavaScript functions, covering functions with parameters and arguments

---
+++ Logica JavaScript/introducao/funcao/introducaoParametro.js
@@ -0,0 +1,43 @@
+
+function teste(str) {
+    console.log(str);
+}
+teste("Olá, mundo!"); // Chamada da função com um argumento
+// Saída: Olá, mundo!
+
+function soma1(a, b) {
+    let soma = a + b;
+    console.log("valor da soma " + soma);
+    // Esta linha nunca será executada
+
+}
+soma1(5, 10);// Chamada da função com dois argumentos
+// Saída: 15
+
+//erros pode ser causados por falta de argumentos
+function soma(a, b) {
+    let soma = a + b;
+    console.log("valor da soma " + soma);
+
+}
+//soma(5); Chamada da função com apenas um argumento
+// Saída: NaN (Not a Number) porque b é undefined     
+
+// erro ecesesso de parametros
+function soma(a, b) {
+    let soma = a + b;
+    console.log("valor da soma " + soma);
+}
+// soma(5, 10, 15); Chamada da função com três argumentos
+// Saída: 15 (o terceiro argumento é ignorado)
+
+//usnado o retono
+function soma2(a, b) {
+    let soma = a + b;
+    return soma; // Retorna o resultado da soma
+}
+  
+o
+let resultado = soma2(5, 10); // Chamada da função com dois argumentos
+// O resultado é armazenado na variável resultado
+console.log("Resultado da soma: " + resultado); // Exibe o resultado
