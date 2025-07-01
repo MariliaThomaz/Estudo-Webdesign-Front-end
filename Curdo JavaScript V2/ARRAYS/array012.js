93ec8b6e274a424c7bea97b29d832df56da84a01
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2025-07-01 18:37:40 -0300

    Uses the filter() method to create a new array containing only numeric elements

--- Curdo JavaScript V2/ARRAYS/array012.js
+++ Curdo JavaScript V2/ARRAYS/array012.js
@@ -1,6 +1,7 @@
 // eu uso o método every() para verificar se todos os elementos de um array são números
 // e o método some() para verificar se pelo menos um elemento do array é um número.
-
+// O método every() e some() são métodos de array que permitem verificar se todos ou pelo menos um elemento do array satisfazem uma condição específica.
+// Eles são muito úteis para validações e verificações em arrays.
 const arr = [1, 5, 10, 'ola', true, null, undefined, 3.14, 'ola', 5];
 //a função every() retorna um valor booleano indicando se todos os elementos do array satisfazem a condição fornecida.
 //Neste caso, estamos verificando se todos os elementos do array são números.
@@ -10,6 +11,23 @@ let somenteNumeros = arr.every(function(eleto){
     // e verificamos se é um número.
     return typeof eleto === 'number';
 })
+
+//fintro par somente variáveis do tipo number
+// o que é feito com o método filter().
+// O método filter() cria um novo array com todos os elementos que passam no teste implementado
+//sua função de callback.
+// Neste caso, estamos filtrando os elementos do array que são números.
+const arr1 = arr.filter(function(eleto, i, _arr){
+
+    return typeof eleto === 'number';
+
+})
+console.log('arr' + arr); // [1, 5, 10, 'ola', true, null, undefined, 3.14, 'ola', 5]
+// A função filter retorna um novo array com os elementos que satisfazem a condição fornecida
+console.log('arr1: '+arr1); // [1, 5, 10, 3.14, 5]
+// A função every retorna true se todos os elementos do array satisfazem a condição fornecida
+ 
+
 console.log("every "+somenteNumeros); // false, pois o array contém strings e booleanos
 
 //A função some() retorna um valor booleano indicando se pelo menos um elemento do array satisfaz a condição fornecida.
