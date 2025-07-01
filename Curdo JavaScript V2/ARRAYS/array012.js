37ab210e6e0ecebff2a7a496f5a6f98cd2bdc4c3
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2025-07-01 19:16:47 -0300

    Studies array methods: forEach() for iteration and map() for creating a new array

--- Curdo JavaScript V2/ARRAYS/array012.js
+++ Curdo JavaScript V2/ARRAYS/array012.js
@@ -17,7 +17,7 @@ let somenteNumeros = arr.every(function(eleto){
 // O método filter() cria um novo array com todos os elementos que passam no teste implementado
 //sua função de callback.
 // Neste caso, estamos filtrando os elementos do array que são números.
-const arr1 = arr.filter(function(eleto, i, _arr){
+const arr1 = arr.filter(function(eleto){
 
     return typeof eleto === 'number';
 
@@ -40,3 +40,19 @@ let temNumeros = arr.some(function(eleto){
 console.log("same " +temNumeros); // true, pois o array contém números
 
 // same faz a mesma coisa que every, mas retorna true se pelo menos um elemento do array satisfaz a condição fornecida.
+
+//faz  uma  interação sobre o array e imprime cada elemento
+arr.forEach(function(eleto){
+    console.log(eleto);
+}) 
+// A função forEach() executa uma função para cada elemento do array
+// Neste caso, estamos imprimindo cada elemento do array no console.
+// A função forEach() não retorna um novo array, apenas executa a função para cada elemento do array.
+
+// A função map() cria um novo array com os resultados da chamada de uma função para cada elemento do array
+// Neste caso, estamos criando um novo array com os elementos do array original multiplicados por 2.
+const arr2 = arr1.map(function(eleto){
+    return eleto * 2;
+})
+console.log('arr2: ' + arr2); // [2, 10, 20, 6.28, 10]
+// A função map() retorna um novo array com os resultados da chamada de uma função para cada    
