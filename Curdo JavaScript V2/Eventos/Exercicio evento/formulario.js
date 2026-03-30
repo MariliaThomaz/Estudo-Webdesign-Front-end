aa3de2e033483c3355e3219d5039bef76c1ad636
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2026-03-30 10:27:33 -0300

    feat: add remaining character counter to textarea using input event

--- Curdo JavaScript V2/Eventos/Exercicio evento/formulario.js
+++ Curdo JavaScript V2/Eventos/Exercicio evento/formulario.js
@@ -3,19 +3,24 @@
 
     const txtTitulo = document.getElementById('txtTitulo')
     const txtDescricao = document.getElementById('txtDescricao')
+    const contadorContainer = document.getElementById('contador')
+    const resta = contadorContainer.getElementsByTagName('span')[0]
+    const maxima = txtDescricao.maxLength
+    mostrarCaracteresDigitados(maxima)
+
     const btnSalvar = document.getElementById('btn')
     const formCadastro = document.querySelector('.formCadastro')
-//oque muda usar o querySelector? Ele é mais flexível, pois permite selecionar elementos 
-// usando seletores CSS, como classes, ids, atributos, etc. Já o getElementById só permite 
-// selecionar elementos pelo id. No caso do formCadastro, como ele tem a classe 
-// 'formCadastro', podemos usar o querySelector para selecioná-lo.
-//  Se usássemos o getElementById, teríamos que dar um id para o formulário, o que não é
-//  necessário nesse caso.
-// qual momento é melhor usar um ou outro? O getElementById é mais rápido, pois é otimizado para
-//  buscar elementos pelo id, que é único. O querySelector é mais flexível, mas 
-// pode ser mais lento, especialmente se o seletor for complexo. Portanto, se você sabe que o
-//  elemento tem um id, é melhor usar o getElementById. Se você precisa selecionar um elemento 
-// por classe, atributo ou outro seletor CSS, use o querySelector.
+    //oque muda usar o querySelector? Ele é mais flexível, pois permite selecionar elementos 
+    // usando seletores CSS, como classes, ids, atributos, etc. Já o getElementById só permite 
+    // selecionar elementos pelo id. No caso do formCadastro, como ele tem a classe 
+    // 'formCadastro', podemos usar o querySelector para selecioná-lo.
+    //  Se usássemos o getElementById, teríamos que dar um id para o formulário, o que não é
+    //  necessário nesse caso.
+    // qual momento é melhor usar um ou outro? O getElementById é mais rápido, pois é otimizado para
+    //  buscar elementos pelo id, que é único. O querySelector é mais flexível, mas 
+    // pode ser mais lento, especialmente se o seletor for complexo. Portanto, se você sabe que o
+    //  elemento tem um id, é melhor usar o getElementById. Se você precisa selecionar um elemento 
+    // por classe, atributo ou outro seletor CSS, use o querySelector.
     formCadastro.addEventListener('submit', function (event) {
 
         event.preventDefault()
@@ -37,5 +42,37 @@
         alert('Tarefa salva com sucesso!')
 
     })
+
+    contadorContainer.removeAttribute('style')
+
+    //Fazer evento
+    /* txtDescricao.addEventListener("keyup", function () {
+         console.log("keyup")
+     })
+     // o keydown é disparado quando a tecla é pressionada, 
+     // o keyup é disparado quando a tecla é solta.
+     txtDescricao.addEventListener("keydown", function () {
+         console.log("keydown")
+     })
+      txtDescricao.addEventListener("keypress", function () {
+          console.log("keypress")
+    })
+    }*/
+    function checkLength() {
+        let caracteresDigitados = txtDescricao.value.length
+        //o parseInt é necessário para converter as strings em números inteiros, para que a subtr   ação funcione corretamente. Se não usássemos o parseInt, a subtração resultaria em uma concatenação de strings, o que não é o comportamento desejado.
+        let caracteresRestantes = parseInt(maxima) - parseInt(caracteresDigitados)
+        resta.textContent = caracteresRestantes
+
+        // const caracteresDigitados = txtDescricao.value.length
+        // const caracteresRestantes = maxima - caracteresDigitados
+        // resta.textContent = caracteresRestantes
+        mostrarCaracteresDigitados(caracteresRestantes)
+    }
+
+    function mostrarCaracteresDigitados(n) {
+        resta.textContent = n
+    }
+    txtDescricao.addEventListener("input", checkLength)
 
 })()
