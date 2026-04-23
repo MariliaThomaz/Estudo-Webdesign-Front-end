76bd93851a59997e35fb2134be03f68e5fe4028a
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2026-04-22 21:17:35 -0300

    study: practice new operator and Date constructor behavior in JavaScript

--- Curdo JavaScript V2/Objetos/objetos.js
+++ Curdo JavaScript V2/Objetos/objetos.js
@@ -30,3 +30,20 @@ const numero = 42;
 const numeroObjeto = new Number(numero);        
 console.log(numeroObjeto); // [Number: 42] // o objeto Number é criado a partir do valor primitivo 42
 console.log(numeroObjeto.valueOf()); // 42  // o método valueOf() retorna o valor primitivo do objeto Number, que é 42  
+
+//operador new ele cria um objeto a partir de uma função construtora, como Number, String, Boolean, etc.
+//mas é importante lembrar que o uso de objetos primitivos como Number, String, Boolean, etc. é desencorajado em JavaScript, 
+//pois pode levar a comportamentos inesperados e confusos. É recomendado usar os tipos primitivos diretamente, como number, string, boolean, etc., 
+//em vez de criar objetos a partir deles.
+// A priedade new ela tem umas  propriedades e métodos que são usados para criar objetos a partir de funções construtoras, como Number, String, Boolean, etc.
+//por exemplo, o operador new é usado para criar um objeto a partir de uma função construtora, como Number, String, Boolean, etc. 
+
+const data = Date(); // cria um objeto Date com a data e hora atuais
+console.log(data); // exibe a data e hora atuais    
+console.log(typeof data); // "object"  // o tipo do objeto Date é "object"
+
+//para  retonar o tipo data tenho que retonar objeto data,
+// este objetio esta contrura Date(), tem cria uma copia do objeto data, e tem acesso a todas as propriedades e métodos do objeto Date, como getFullYear(), getMonth(), getDate(), etc.
+const dataObjeto = new Date(data);  
+console.log(dataObjeto); // exibe a data e hora atuais como um objeto Date
+console.log(typeof dataObjeto); // "object"  // o tipo do objeto Date é "object"    
